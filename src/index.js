f32a3d91ca755ca7a392d0435dd11219b6398c27
kyawlinntun-gif <kyawlinntun100@gmail.com>
2022-05-19 09:13:19 +0630

    React components

--- src/index.js
+++ src/index.js
@@ -1,17 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import CommentDetail from "./CommentDetail";
+import faker from "@faker-js/faker";
+import ApprovalCard from "./ApprovalCard";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const App = () => {
+    return (
+        <div className="ui comments">
+            <ApprovalCard>
+                <div>
+                    <h4>Warning!</h4>
+                    <p>Are you sure you want to do this?</p>
+                </div>
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                author="Aung Aung" 
+                timeAgo="Today at 2:30PM"
+                content="Nice blog post!"
+                avatar={faker.image.avatar()} />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                author="Tun Tun" 
+                timeAgo="Today at 6:00PM"
+                content="I like the subject."
+                avatar={faker.image.avatar()} />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                author="Sam" 
+                timeAgo="Yesterday at 5:00AM"
+                content="I like the writing."
+                avatar={faker.image.avatar()} />
+            </ApprovalCard>
+        </div>
+    )
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
